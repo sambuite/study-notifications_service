f236b9d664f3d41e2386149a3cd15f8f5d55a735
sambuite <murilosambuite@gmail.com>
2022-12-16 21:35:21 -0300

    create send-notification; organize files and folders

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,18 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+import { SendNotification } from './send-notification';
+
+describe('Send notification', () => {
+  it('should be able to send notification', async () => {
+    const notificationRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationRepository);
+
+    const { notification } = await sendNotification.execute({
+      recipientId: 'test-recipientId',
+      content: 'This is a notification',
+      category: 'social',
+    });
+
+    expect(notificationRepository.notifications).toHaveLength(1);
+    expect(notificationRepository.notifications[0]).toEqual(notification);
+  });
+});
